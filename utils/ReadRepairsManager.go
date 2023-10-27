287ad663d04d9db6149a5ee0a248ec99de2fcc54
Ryan Toh <ryedralisk@gmail.com>
2023-10-27 19:10:36 +0800

    feat: alternative read repair (see comments)

--- utils/ReadRepairsManager.go
+++ utils/ReadRepairsManager.go
@@ -1,149 +1,146 @@
+// Commented so the system can run, this is still not converted to Ryan's version since he did the variant in func readRepair in read.go (line 94) 
+
 package utils
 
-import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-type ReplicaData struct {
-	filepath string
-	data Database
-}
-
-type RowDiscrepancy struct {
-	NodeId      NodeId
-	CurrData    Row
-	CorrectData Row
-}
-
-type ReadRepairsManager struct {
-	mux           sync.Mutex
-	Discrepancies []RowDiscrepancy
-	Responses     []Response
-}
-
-//----------------------------------------
-// Print status??
-//----------------------------------------
-
-// func (rrm ReadRepairsManager) String() string {
-
+// type ReplicaData struct {
+// 	filepath string
+// 	data Database
 // }
 
-//----------------------------------------
-// Constructor
-//----------------------------------------
+// type RowDiscrepancy struct {
+// 	NodeId      NodeId
+// 	CurrData    Row
+// 	CorrectData Row
+// }
 
-func NewReadRepairsManager(responses []Response) *ReadRepairsManager {
-	return &ReadRepairsManager{
-		Discrepancies: make([]RowDiscrepancy, 0),
-		Responses:     responses,
-	}
-}
+// type ReadRepairsManager struct {
+// 	mux           sync.Mutex
+// 	Discrepancies []RowDiscrepancy
+// 	Responses     []Response
+// }
 
-//----------------------------------------
-// Methods
-//----------------------------------------
-func (rrm *ReadRepairsManager) PerformReadRepair(responses []Response) {
-	// If there are no responses, there is nothing to repair
-	if len(responses) == 0 {
-		return
-	}
+// //----------------------------------------
+// // Print status??
+// //----------------------------------------
 
-	var latestData *Row
-	var latestTimestamp int64
-	var validResponses int
+// // func (rrm ReadRepairsManager) String() string {
 
-	// Identify the latest data
-	for _, response := range responses {
-		// If the response is not valid, skip it
-		if response.Error != nil {
-			// TODO : log error in response
-			// TODO : sent to repair in for loop below, not sure if should
-			continue
-		}
+// // }
 
-		// handle or log nil data
-		if response.Data == nil {
-			// TODO : log error in response
-			// TODO : sent to repair in for loop below, not sure if should
-			continue
-		}
+// //----------------------------------------
+// // Constructor
+// //----------------------------------------
 
-		// If the response is valid, increment the number of valid responses
-		validResponses++
+// func NewReadRepairsManager(responses []Response) *ReadRepairsManager {
+// 	return &ReadRepairsManager{
+// 		Discrepancies: make([]RowDiscrepancy, 0),
+// 		Responses:     responses,
+// 	}
+// }
 
-		// If the response is the latest data, update the latest data
-		if response.Data.CreatedAt > latestTimestamp {
-			latestTimestamp = response.Data.CreatedAt
-			latestData = response.Data
-		}
-	}
+// //----------------------------------------
+// // Methods
+// //----------------------------------------
+// func (rrm *ReadRepairsManager) PerformReadRepair(responses []Response) {
+// 	// If there are no responses, there is nothing to repair
+// 	if len(responses) == 0 {
+// 		return
+// 	}
 
-	// if there are no valid responses, there is nothing to repair
-	if validResponses == 0 {
-		return
-	}
+// 	var latestData *Row
+// 	var latestTimestamp int64
+// 	var validResponses int
+
+// 	// Identify the latest data
+// 	for _, response := range responses {
+// 		// If the response is not valid, skip it
+// 		if response.Error != nil {
+// 			// TODO : log error in response
+// 			// TODO : sent to repair in for loop below, not sure if should
+// 			continue
+// 		}
+
+// 		// handle or log nil data
+// 		if response.Data == nil {
+// 			// TODO : log error in response
+// 			// TODO : sent to repair in for loop below, not sure if should
+// 			continue
+// 		}
+
+// 		// If the response is valid, increment the number of valid responses
+// 		validResponses++
+
+// 		// If the response is the latest data, update the latest data
+// 		if response.Data.CreatedAt > latestTimestamp {
+// 			latestTimestamp = response.Data.CreatedAt
+// 			latestData = response.Data
+// 		}
+// 	}
+
+// 	// if there are no valid responses, there is nothing to repair
+// 	if validResponses == 0 {
+// 		return
+// 	}
 	
-	// For each response, check if it matches the latest data
-	for _, response := range responses {
-		if response.Data.CreatedAt != latestTimestamp || response.Data == nil || response.Error != nil {
-			discrepancy := RowDiscrepancy{
-				NodeId:      response.NodeId,
-				CurrData:    *response.Data,
-				CorrectData: *latestData,
-			}
-			rrm.Discrepancies = append(rrm.Discrepancies, discrepancy)
-		}
-	}
+// 	// For each response, check if it matches the latest data
+// 	for _, response := range responses {
+// 		if response.Data.CreatedAt != latestTimestamp || response.Data == nil || response.Error != nil {
+// 			discrepancy := RowDiscrepancy{
+// 				NodeId:      response.NodeId,
+// 				CurrData:    *response.Data,
+// 				CorrectData: *latestData,
+// 			}
+// 			rrm.Discrepancies = append(rrm.Discrepancies, discrepancy)
+// 		}
+// 	}
 
-	// Handle the discrepancies
-	rrm.HandleDiscrepancies()
-}
+// 	// Handle the discrepancies
+// 	rrm.HandleDiscrepancies()
+// }
 
-func (rrm *ReadRepairsManager) HandleDiscrepancies() {
-	// If there are no discrepancies, there is nothing to repair
-	if len(rrm.Discrepancies) == 0 {
-		fmt.Println("[RRM] No discrepancies to repair")
-		return
-	}
+// func (rrm *ReadRepairsManager) HandleDiscrepancies() {
+// 	// If there are no discrepancies, there is nothing to repair
+// 	if len(rrm.Discrepancies) == 0 {
+// 		fmt.Println("[RRM] No discrepancies to repair")
+// 		return
+// 	}
 
-	// Create a list to store write requests
-	writeRequests := []Request{}
+// 	// Create a list to store write requests
+// 	writeRequests := []Request{}
 
-	// For each discrepancy, send a write request to the node with the latest data
-	for _, discrepancy := range rrm.Discrepancies {
-		writeURL := fmt.Sprintf(WRITE_ENDPOINT_FSTRING, discrepancy.NodeId, discrepancy.CorrectData.CourseId, discrepancy.CorrectData.StudentId)
+// 	// For each discrepancy, send a write request to the node with the latest data
+// 	for _, discrepancy := range rrm.Discrepancies {
+// 		writeURL := fmt.Sprintf(WRITE_ENDPOINT_FSTRING, discrepancy.NodeId, discrepancy.CorrectData.CourseId, discrepancy.CorrectData.StudentId)
 
-		// Create a write request
-		writeRequest := Request{
-			NodeId:  discrepancy.NodeId,
-			Url:     writeURL,
-			Payload: &discrepancy.CorrectData,
-		}
+// 		// Create a write request
+// 		writeRequest := Request{
+// 			NodeId:  discrepancy.NodeId,
+// 			Url:     writeURL,
+// 			Payload: &discrepancy.CorrectData,
+// 		}
 
-		// Add the write request to the list of write requests
-		writeRequests = append(writeRequests, writeRequest)
-	}
+// 		// Add the write request to the list of write requests
+// 		writeRequests = append(writeRequests, writeRequest)
+// 	}
 
-	// Send the write requests
-	responses := IntraSystemRequests(writeRequests)
+// 	// Send the write requests
+// 	responses := IntraSystemRequests(writeRequests)
 
-	// Handle the responses
-	for _, response := range responses {
-		if response.Error != nil {
-			fmt.Println("[RRM] Error in repairing node ", response.NodeId, ": ", response.Error)
-			// TODO : consider adding to hinted handoff or another mechanism to repair in the future
-		} else {
-			fmt.Println("[RRM] Successfully repaired node ", response.NodeId, " with data: ", response.Data)
-		}
-	}
+// 	// Handle the responses
+// 	for _, response := range responses {
+// 		if response.Error != nil {
+// 			fmt.Println("[RRM] Error in repairing node ", response.NodeId, ": ", response.Error)
+// 			// TODO : consider adding to hinted handoff or another mechanism to repair in the future
+// 		} else {
+// 			fmt.Println("[RRM] Successfully repaired node ", response.NodeId, " with data: ", response.Data)
+// 		}
+// 	}
 
-	// Clear the discrepancies
-	rrm.Discrepancies = []RowDiscrepancy{}
-}
+// 	// Clear the discrepancies
+// 	rrm.Discrepancies = []RowDiscrepancy{}
+// }
